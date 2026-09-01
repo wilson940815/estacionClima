88af7d27907e1efbda3d8fd86c4e4e07fd808411
wilson940815 <147207696+wilson940815@users.noreply.github.com>
2026-08-31 21:11:01 -0500

    Update page title in app_nivel_cornare.py

--- app_nivel_cornare.py
+++ app_nivel_cornare.py
@@ -30,7 +30,7 @@ LLAVE_VALOR = "level"
 CANDIDATOS_LAT = ["lat", "latitude", "latitud"]
 CANDIDATOS_LON = ["lng", "lon", "longitude", "longitud"]
 
-st.set_page_config(page_title="Nivel de estación — CORNARE", page_icon="🌊", layout="wide")
+st.set_page_config(page_title="Concepción, Río Concepción, Barrio Obrero", page_icon="🌊", layout="wide")
 
 
 # ------------------------------------------------------------------
